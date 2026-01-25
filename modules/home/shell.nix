e23bd7a0405b349e7005084af7228815ea36d425
Saul German Gutierrez Calderon <me@sggc.me>
2026-01-25 14:46:24 -0800

    [ricing] make use of starship in terminal prompt

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -34,11 +34,78 @@
     '';
   };
 
-  # Starship prompt (optional, uncomment to enable)
-  # programs.starship = {
-  #   enable = true;
-  #   enableZshIntegration = true;
-  # };
+  # Starship prompt
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      format = lib.concatStrings [
+        "[#](bold blue) "
+        "$username"
+        " @ "
+        "$hostname"
+        " in "
+        "$directory"
+        "$git_branch"
+        "$git_status"
+        " "
+        "\\[$time\\]"
+        "$status"
+        "$line_break"
+        "$character"
+      ];
+
+      username = {
+        show_always = true;
+        format = "[$user](cyan)";
+      };
+
+      hostname = {
+        ssh_only = false;
+        format = "[$hostname](green)";
+      };
+
+      directory = {
+        format = "[$path](bold yellow)";
+        truncation_length = 3;
+        truncate_to_repo = false;
+      };
+
+      git_branch = {
+        format = "[ on](white) [git:](bold white)[$branch](bold purple)";
+      };
+
+      git_status = {
+        format = "[ $all_status$ahead_behind](bold yellow)";
+        conflicted = "=";
+        ahead = "⇡";
+        behind = "⇣";
+        diverged = "⇕";
+        untracked = "?";
+        stashed = "$";
+        modified = "!";
+        staged = "+";
+        renamed = "»";
+        deleted = "✘";
+      };
+
+      time = {
+        disabled = false;
+        format = "$time";
+        time_format = "%T";
+      };
+
+      status = {
+        disabled = false;
+        format = "[ C:$status](bold red)";
+      };
+
+      character = {
+        success_symbol = "[\\$](bold red)";
+        error_symbol = "[\\$](bold red)";
+      };
+    };
+  };
 
   # Direnv for per-directory environments
   programs.direnv = {
